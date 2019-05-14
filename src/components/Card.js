2aa070df3dbf68972425892fd10011dcdf32f10a
Your Name <you@example.com>
2019-05-13 21:05:21 -0700

    reorg

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Card = (props) => {
+  console.log('card: ', props)
+
+  return (
+    <div>
+      {props.isList}
+    </div>
+  );
+};
+
+export default Card;
